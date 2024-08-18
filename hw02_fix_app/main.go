20b247381401d0dd600ea7e3b77ecaa44c3d3bfc
Andrei <andrew.a.rubin@hotmail.com>
2024-08-18 11:49:43 +0200

    HW02

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,29 +1,53 @@
-package init
+package main
 
 import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/fixme_my_friend/hw02_fix_app/employee"
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
-	"fmt"
 )
 
+const fileName = "data.json"
+
 func init() {
-	var path string = "data.json"
-
-	fmt.Printf("Enter data file path: ")
-	fmt.Scanln(&path)
-
 	var err error
-	var staff []types.Employee
+	var staff []employee.Employee
+	staff, err = reader.ReadJSON(getFilePath(fileName))
+	if err != nil {
+		fmt.Println(err)
+	}
+	printer.PrintStaff(staff)
+}
 
-	if len(path) == 0 {
-		path = "data.json"
-	} else {
+func getFilePath(fileNameDefault string) string {
+	var path string
+	fmt.Printf("Enter data file path: ")
+	_, err := fmt.Scanln(&path)
+	if err != nil {
+		return getPathByDefault(fileNameDefault)
+	}
+	if strings.TrimSpace(path) == "" {
+		path = getPathByDefault(fileNameDefault)
+	}
+	return path
+}
+
+func getPathByDefault(fileName string) string {
+	_, filename, _, ok := runtime.Caller(0)
+
+	if !ok {
+		fmt.Println("Error get the path to the source file")
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
+	directory := filepath.Dir(filename)
+	fmt.Println(directory)
+	return filepath.Join(directory, fileName)
+}
 
-	fmt.Print(err)
-
-	printer.PrintStaff(staff)
+func main() {
+	fmt.Print("Done ...")
 }
---
+++ hw02_fix_app/reader/reader.go
@@ -0,0 +1,34 @@
+package reader
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/fixme_my_friend/hw02_fix_app/employee"
+	"io"
+	"os"
+)
+
+func ReadJSON(filePath string) ([]employee.Employee, error) {
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+
+	bytes, err := io.ReadAll(f)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return nil, nil
+	}
+
+	var data []employee.Employee
+
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling json: %w", err)
+	}
+
+	res := data
+
+	return res, nil
+}
