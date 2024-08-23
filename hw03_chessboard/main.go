cbbaeafdff2d07f0cb8a883d5d3b0ee3f84188e0
Andrei <andrew.a.rubin@hotmail.com>
2024-08-23 17:38:19 +0200

    HW03 board chess

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,70 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	SPACE_COUNT = 1
+	SPACE_TYPE  = "\t"
+)
+
+func printBoard(height int, length int) {
+	for i := 0; i <= height+1; i++ {
+		if i == 0 {
+			printHeader(length)
+		} else if i <= height {
+			printRowBoundary(length)
+			printRow(i, length)
+		} else {
+			printFooter(height, length)
+		}
+		fmt.Println("")
+	}
+}
+
+func printHeader(length int) {
+	printLetter(length)
+}
+
+func printLetter(length int) {
+	printTub()
+	for j := 0; j < length; j++ {
+		letter := byte('A') + byte(j)
+		fmt.Printf("  %c ", letter)
+	}
+}
+
+func printRowBoundary(length int) {
+	printTub()
+	fmt.Println(strings.Repeat(" ---", length))
+}
+
+func printRow(rowNum int, length int) {
+	fmt.Print(rowNum)
+	printTub()
+	fmt.Print("|")
+	for j := 0; j < length; j++ {
+		if (rowNum+j)%2 == 0 {
+			fmt.Print("   |")
+		} else {
+			fmt.Print(" X |")
+		}
+	}
+	printTub()
+	fmt.Print(rowNum)
+}
+
+func printFooter(_ int, length int) {
+	printRowBoundary(length)
+	printLetter(length)
+}
+
+func printTub() {
+	fmt.Print(strings.Repeat(SPACE_TYPE, SPACE_COUNT))
+}
+
 func main() {
-	// Place your code here.
+	printBoard(8, 8)
 }
