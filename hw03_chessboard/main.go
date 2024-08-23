70acf38a4ba3bf2f287465f64c86509b1da6c51b
Andrei <andrew.a.rubin@hotmail.com>
2024-08-23 20:27:40 +0200

    HW03 add scanner

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
+)
+
+var (
+	height int
+	length int
 )
 
 const (
 	spaceCount = 1
 	spaceType  = "\t"
+	msgHeight  = "Please enter board height (from 2 to 10)"
+	msgLength  = "Please enter board length (from 2 to 10)"
 )
+
+func init() {
+	start()
+}
 
 func printBoard(height int, length int) {
 	for i := 0; i <= height+1; i++ {
@@ -66,6 +80,46 @@ func printTub() {
 	fmt.Print(strings.Repeat(spaceType, spaceCount))
 }
 
+func convertParam(param string) (i int, err error) {
+	intParam, err := strconv.Atoi(param)
+	if err != nil {
+		return 0, err
+	}
+	return intParam, nil
+}
+
+func readPromptValue(prompt string) string {
+	fmt.Println(prompt)
+	reader := bufio.NewReader(os.Stdin)
+	value, _ := reader.ReadString('\n')
+	return strings.TrimSpace(value)
+}
+
+func getBoardParam(param *int, msg string) {
+	var err error
+	count := 0
+	for {
+		value := readPromptValue(msg)
+		*param, err = convertParam(value)
+		if count > 2 {
+			fmt.Println("You put more then 3 time wrong value \n program exit")
+			os.Exit(0)
+		} else if err != nil || *param < 2 || *param > 10 {
+			fmt.Println("You putted wrong value")
+			count++
+			continue
+		}
+		break
+	}
+}
+
+func start() {
+	getBoardParam(&height, msgHeight)
+	getBoardParam(&length, msgLength)
+}
+
 func main() {
-	printBoard(8, 8)
+	if height > 0 && length > 0 {
+		printBoard(height, length)
+	}
 }
