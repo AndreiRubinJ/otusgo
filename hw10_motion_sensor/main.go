77458b1e81237a8f23156538e4df063591c1933d
Andrei <andrew.a.rubin@hotmail.com>
2024-10-29 19:15:30 +0100

    hw10_motion_sensor

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,5 +1,74 @@
 package main
 
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"time"
+)
+
 func main() {
-	// Place your code here.
+	sensorDataChan := make(chan float64)
+	processedDataChan := make(chan float64)
+
+	go simulateSensorRead(sensorDataChan)
+	go processSensorData(sensorDataChan, processedDataChan)
+
+	for average := range processedDataChan {
+		fmt.Printf("Received average: %f\n", average)
+	}
+}
+
+func simulateSensorRead(sensorDataChan chan float64) {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	timeout := time.After(time.Minute)
+	for {
+		select {
+		case <-timeout:
+			close(sensorDataChan)
+			return
+		case <-ticker.C:
+			value, err := randFloat64()
+			if err != nil {
+				fmt.Println("Error generating random number:", err)
+				close(sensorDataChan)
+				return
+			}
+			sensorDataChan <- value * 100
+		}
+	}
+}
+
+func processSensorData(sensorDataChan, processedDataChan chan float64) {
+	dataBatch := make([]float64, 0, 10)
+	for data := range sensorDataChan {
+		dataBatch = append(dataBatch, data)
+		if len(dataBatch) == 10 {
+			var sum float64
+			for _, value := range dataBatch {
+				sum += value
+			}
+			average := sum / float64(len(dataBatch))
+			processedDataChan <- average
+			dataBatch = []float64{}
+		}
+	}
+	if len(dataBatch) > 0 {
+		var sum float64
+		for _, value := range dataBatch {
+			sum += value
+		}
+		average := sum / float64(len(dataBatch))
+		processedDataChan <- average
+	}
+	close(processedDataChan)
+}
+func randFloat64() (float64, error) {
+	n, err := rand.Int(rand.Reader, big.NewInt(100))
+	if err != nil {
+		return 0, err
+	}
+	return float64(n.Int64()), nil
 }
---
+++ hw10_motion_sensor/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessSensorData(t *testing.T) {
+	sensorDataChan := make(chan float64)
+	processedDataChan := make(chan float64)
+
+	go processSensorData(sensorDataChan, processedDataChan)
+
+	// Отправляем 20 значений в канал
+	go func() {
+		for i := 1; i <= 20; i++ {
+			sensorDataChan <- float64(i)
+		}
+		close(sensorDataChan)
+	}()
+
+	// Ожидаемые средние значения: (1+2+...+10)/10 и (11+12+...+20)/10
+	expectedAverages := []float64{5.5, 15.5}
+
+	var receivedAverages []float64
+	for {
+		select {
+		case avg, ok := <-processedDataChan:
+			if !ok {
+				assert.Equal(t, expectedAverages, receivedAverages, "Expected and received averages do not match")
+				return
+			}
+			receivedAverages = append(receivedAverages, avg)
+		case <-time.After(2 * time.Second):
+			t.Fatal("Timeout waiting for processed data")
+		}
+	}
+}
