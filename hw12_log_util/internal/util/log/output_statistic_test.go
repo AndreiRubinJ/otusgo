029a860871ff4f19f45b12ced86191cf5ac138c5
Andrei <andrew.a.rubin@hotmail.com>
2024-12-08 22:14:35 +0100

    hw12_log_util

--- hw12_log_util/internal/util/log/output_statistic_test.go
+++ hw12_log_util/internal/util/log/output_statistic_test.go
@@ -46,22 +46,17 @@ func TestOutputToConsole(t *testing.T) {
 		},
 	}
 
-	// Create a pipe to capture standard output
 	r, w, err := os.Pipe()
 	if err != nil {
 		t.Fatalf("failed to create pipe: %v", err)
 	}
 
-	// Save the original stdout so we can restore it later
 	originalStdout := os.Stdout
 	defer func() { os.Stdout = originalStdout }()
 
-	// Set os.Stdout to the write end of the pipe
 	os.Stdout = w
 
-	// Create a channel to capture the output asynchronously
 	outC := make(chan string)
-	// Capture stdout in a goroutine
 	go func() {
 		var buf bytes.Buffer
 		io.Copy(&buf, r)
